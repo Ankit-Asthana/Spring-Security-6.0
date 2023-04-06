226c86d33d00db6914f9fcdda9731a64cc854795
Ankit-Asthana <ankitasthana486@gmail.com>
2023-04-06 15:43:46 +0530

    Spring 6.0

---
+++ src/main/java/com/ankit/controller/AuthenticationService.java
@@ -0,0 +1,53 @@
+package com.ankit.controller;
+
+import com.ankit.model.Role;
+import com.ankit.model.User;
+import com.ankit.repository.UserRepository;
+import com.ankit.security.AuthenticationResponse;
+import com.ankit.security.JwtService;
+import lombok.RequiredArgsConstructor;
+//import lombok.var;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user= User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        repository.save(user);
+        var jwtToken=jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user= repository.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken=jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
---
+++ src/main/java/com/ankit/model/Role.java
@@ -0,0 +1,6 @@
+package com.ankit.model;
+
+public enum Role {
+    USER,
+    ADMIN
+}
